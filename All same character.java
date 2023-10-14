62da7e4adca2c3af52e3039560b88ef3d32277e7
Antoflomine <68322807+Antoflomine@users.noreply.github.com>
2023-10-14 20:14:58 +0530

    Create All same character.java

---
+++ All same character.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Your Code Here
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		char r=str.charAt(0);
+		int c=0;
+		for(int i=0;i<str.length();i++){
+		    if(r==str.charAt(i)){
+		        c++;
+		    }
+		}
+		if(c==str.length()){
+		System.out.print("YES");
+		}
+		else{
+		    System.out.print("NO");
+		}
+
+	}
+}
