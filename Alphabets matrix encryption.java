e0b590804fc33777a7c8b4a094b613eb17372671
Antoflomine <68322807+Antoflomine@users.noreply.github.com>
2025-03-02 10:56:27 +0530

    Create Alphabets matrix encryption.java

---
+++ Alphabets matrix encryption.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String[][] arr=new String[5][5];
+		for(int i=0;i<5;i++){
+		    for(int j=0;j<5;j++){
+		        arr[i][j]=sc.next();
+		    }
+		}
+		sc.nextLine();
+		String s=sc.nextLine();
+		StringBuilder res=new StringBuilder();
+		int k=0;
+		for(int z=0;z<s.length();z++){
+		    String e=String.valueOf(s.charAt(z));
+		    int flag=0;
+		for(int i=0;i<5;i++){
+		    for(int j=0;j<5;j++){
+		        if(arr[i][j].contains(e)){
+		            res.append(i+1);
+		            res.append(j+1);
+		            flag=1;
+		        }
+		    }
+		}
+		    if(flag==0){
+		        res.append(" ");
+		    
+		       
+		    
+		}
+		}
+		System.out.println(res);
+		    
+		
+
+	}
+}
