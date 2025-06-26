afc2bf58f0eed1833119a6f180003907800368bb
Antoflomine <68322807+Antoflomine@users.noreply.github.com>
2025-06-26 11:03:49 +0530

    Create AlphabetBitMasking.java

---
+++ AlphabetBitMasking.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    String str=sc.nextLine();
+	    Set<Character> set=new HashSet<>();
+		for(int i=0;i<str.length();i++){
+		    set.add(str.charAt(i));
+		}
+		//System.out.println(set);
+		ArrayList<Character> as=new ArrayList<>(set);
+		
+		int[] arr=new int[26];//a-0 b-1 c-2 ...z-25 a-97 
+		for(int i=0;i<set.size();i++){
+		    arr[((int)(as.get(i)))-97]++;
+		}
+		String res="";
+		for(int i=25;i>=0;i--){
+		    res+=arr[i];
+		}
+		System.out.println(Integer.parseInt(res,2));
+		
+	}
+}
